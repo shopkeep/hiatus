026431a1ef58af6d37e87047e17921dfef7823df
Alex Gessner <gessner@shopkeep.com>
2014-03-17 17:08:44 -0400

    spike

--- lib/hiatus.rb
+++ lib/hiatus.rb
@@ -22,4 +22,20 @@ module Hiatus
   def self.update(name, time)
     @storage.setex(namespace(name), time, 'on hiatus')
   end
+
+  module KillSwitch
+    def pause(seconds = 1800)
+      Hiatus.pause(key, seconds)
+    end
+
+    def paused?
+      !!Hiatus.paused?(key)
+    end
+
+    private
+
+    def key
+      self.to_s.downcase
+    end
+  end
 end
